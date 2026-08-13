d08fa73b96d7bbc49e6dad3dabf60718c753acf5
Rajshree <rajshreebhuyan1306@gmail.com>
2026-08-13 16:10:48 +0530

    array accumulate

---
+++ groupsalary.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int m; cin>>m;
+        int arr[m];
+        for(int i=0;i<m;i++)    cin>>arr[i];
+        int sum=accumulate(arr, arr+m, 0);
+        cout<<sum<<endl;
+    }
+}
