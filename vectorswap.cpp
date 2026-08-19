9a3c8ce9cfc540eb922270b8d71fd3288c65fdd0
Rajshree <rajshreebhuyan1306@gmail.com>
2026-08-19 13:30:55 +0530

    swapping

---
+++ vectorswap.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,k; cin>>n>>k;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)   cin>>v[i];
+	for(int i=0;i<n;i+=2*k){
+	    for(int j=i;j<i+k;j++){
+	        std::swap(v[j], v[j+k]);
+	    }
+	}
+	for(int i=0;i<n;i++)   cout<<v[i]<<" ";
+
+}
