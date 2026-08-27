d491ca8e82542053d691ca30a630bb83e9bf3bda
Rajshree <rajshreebhuyan1306@gmail.com>
2026-08-27 13:43:47 +0530

    compare arrays

---
+++ compare.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,m; cin>>n>>m;
+	vector<int> v(n);
+	vector<int> u(m);
+	for(int i=0;i<n;i++)    cin>>v[i];
+	for(int i=0;i<m;i++)    cin>>u[i];
+	for(int i=0;i<m;i++){
+	    int c=0;
+	    for(int j=0;j<n;j++){
+	        if (v[j]>u[i])  c++;
+	    }
+	    cout<<c<<" ";
+	}
+
+}
