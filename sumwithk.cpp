6b33a12e44f0e8462a0322720c07deb3dfd867ba
Rajshree <rajshreebhuyan1306@gmail.com>
2026-08-27 14:05:34 +0530

    all pairs with sum k

---
+++ sumwithk.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,m; cin>>n>>m;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)    cin>>v[i];
+	for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (v[i] + v[j] == m) {
+                cout << v[i] << " " << v[j] << endl;
+            }
+        }
+    }
+
+}
