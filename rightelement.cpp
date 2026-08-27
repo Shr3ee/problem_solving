0c3e3c0ba21ff4b30b79e55a2015d568c25b05fe
Rajshree <rajshreebhuyan1306@gmail.com>
2026-08-27 14:25:26 +0530

    next greater right element

---
+++ rightelement.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)    cin>>v[i];
+	for (int i=0;i<n;i++) {
+	    int max=v[i];
+        for (int j=i+1;j<n;j++) {
+            if (v[j]>max)   max=v[j];
+        }
+        if (max==v[i])  cout<<0<<" ";
+        else cout<<max<<" ";
+    }
+
+}
