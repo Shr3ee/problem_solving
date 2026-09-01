f25778e1812bd27e7565c0a110c719c57542ecf9
Rajshree <rajshreebhuyan1306@gmail.com>
2026-09-01 16:02:41 +0530

    swap matrix elements

---
+++ swapMatrixElements.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> mat(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m - 1; j++) {
+            swap(mat[i][j], mat[i + 1][j + 1]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << mat[i][j];
+
+            if (j != m - 1)
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
