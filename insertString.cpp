7d3fa42601200b9ce951c02946feb1e1ed921f8e
Rajshree <rajshreebhuyan1306@gmail.com>
2026-09-02 13:22:04 +0530

    insert string at interval

--- insertString.cpp
+++ insertString.cpp
@@ -4,20 +4,16 @@ using namespace std;
 int main() {
     string s, x;
     int k;
-
     getline(cin,s);
-    cin >> x;
-    cin >> k;
-
-    string result = "";
-    for (int i = 0; i < s.length(); i++) {
-        result += s[i];
-        if ((i + 1) % k == 0 && i != s.length() - 1) {
+    cin>>x;
+    cin>>k;
+    string result="";
+    for (int i=0;i<s.length();i++) {
+        result+=s[i];
+        if ((i + 1)%k==0 && i!=s.length()-1) {
             result += x;
         }
     }
-
-    cout << result;
-
+    cout<<result;
     return 0;
 }
