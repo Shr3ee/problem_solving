271dcf5b8af854872c974f1350a99895ade7927b
Rajshree <rajshreebhuyan1306@gmail.com>
2026-09-10 15:10:57 +0530

    heh

--- pairedstrings.cpp
+++ pairedstrings.cpp
@@ -10,7 +10,7 @@ int main() {
     int l=v[0].length();
     for(int i=0;i<n/2;i++){
         string s=v[i].substr(0,i+1)+v[n-1-i].substr(l-i-1,i+1);
-        cout<<s<<" ";
+        cout<<s;
+        if (i<n/2-1)    cout<<" ";
     }
-    
 }
